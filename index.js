45e3aa59bb2ac026d991ac286688de1eb0c7448e
Purity Mwaura <purity.mwaura@student.moringaschool.com>
2022-06-22 15:18:14 +0300

    added an API key and also images url

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * Unique API Key for TMDB Endpoints requests
+ */
+ const API_KEY = '9dfd01779b6fdeb1cde19f1c010bb6a9';
+ /**
+  * TMDB TV Series Endpoints URLs
+  */
+ const APIURL = `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}`;
+ const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+ const SEARCHAPI =
+ `https://api.themoviedb.org/3/search/tv?api_key=${API_KEY}&query=`;
